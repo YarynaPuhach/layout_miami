a4c6d1ac2ffbe198e93dce960c958d5c016f74a9
YarynaPuhach <puhachyaryna@gmail.com>
2024-02-12 12:46:39 +0100

    make build

---
+++ index.aabfa57c.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page--with-menu"):document.body.classList.remove("page--with-menu")});
+//# sourceMappingURL=index.aabfa57c.js.map
